59c99a50490ad11c1319c272ec8aaa91486b2762
Jean Paul Bernhardt <beralb@protonmail.com>
2023-02-19 14:09:55 -0300

    fix: fix 02 function test_count

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,7 +4,7 @@ from src.pre_built.counter import count_ocurrences
 
 def test_count():
     path = "data/jobs.csv"
-    word = "developer"
+    word = "python"
     count_python = count_ocurrences(path, word)
 
     assert count_python == 1639
