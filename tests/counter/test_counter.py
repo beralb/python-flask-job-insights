abecabf0ef8fe260a306893a7fd6e02a6300d323
Jean Paul Bernhardt <beralb@protonmail.com>
2023-02-19 13:58:53 -0300

    fix: function test_count

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,8 +3,8 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_count():
-    count_python = count_ocurrences("data/jobs.csv", "python")
-    assert count_python == 1639
+    path = "data/jobs.csv"
+    word = "developer"
+    count_python = count_ocurrences(path, word)
 
-    count_js = count_ocurrences("data/jobs.csv", "javascript")
-    assert count_js == 122
+    assert count_python == 1639
