31654fe33b66e0508b5bbf79ca9fc3a58ab2af1e
Jean Paul Bernhardt <beralb@protonmail.com>
2023-02-19 19:05:41 -0300

    feat: function test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,72 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    """
+    Create a small sample of jobs
+    """
+    jobs = [
+        {
+            "title": "Maquinista",
+            "min_salary": 2000,
+            "max_salary": 3000,
+            "date_posted": "2022-12-20",
+        },
+        {
+            "title": "Analista de Software",
+            "min_salary": 4000,
+            "max_salary": 5000,
+            "date_posted": "2022-12-22",
+        },
+        {
+            "title": "Motorista",
+            "min_salary": 3000,
+            "max_salary": 4000,
+            "date_posted": "2022-12-23",
+        },
+        {
+            "title": "Auxiliar administrativo",
+            "min_salary": 1700,
+            "max_salary": 2500,
+            "date_posted": "2022-12-24",
+        },
+    ]
+
+    """
+    Sort the jobs by different criteria and check the result
+    """
+    sort_by(jobs, "min_salary")
+    assert [job["title"] for job in jobs] == [
+        "Auxiliar administrativo",
+        "Maquinista",
+        "Motorista",
+        "Analista de Software",
+    ]
+
+    sort_by(jobs, "max_salary")
+    assert [job["title"] for job in jobs] == [
+        "Analista de Software",
+        "Motorista",
+        "Maquinista",
+        "Auxiliar administrativo",
+    ]
+
+    sort_by(jobs, "date_posted")
+    assert [job["title"] for job in jobs] == [
+        "Auxiliar administrativo",
+        "Motorista",
+        "Analista de Software",
+        "Maquinista",
+    ]
+
+    """
+    Check that jobs without valid values for chosen criteria appear at end
+    """
+    jobs[1]["max_salary"] = None
+    sort_by(jobs, "max_salary")
+    assert [job["title"] for job in jobs] == [
+        "Analista de Software",
+        "Maquinista",
+        "Auxiliar administrativo",
+        "Motorista",
+    ]
